d27295cd8cd9bbac56046b6c351b5805ca35ce19
realShengYao <30943636+realShengYao@users.noreply.github.com>
2025-02-20 07:14:17 +0000

    Deploying to gh-pages from @ realShengYao/realShengYao.github.io@8d2be4e292be38342211d1d0b090481c19f4a11a 🚀

--- _pages/about_einstein.md
+++
@@ -1,9 +0,0 @@
-I'm a Computer Science and Engineering Ph.D. student at <a href='https://hkust.edu.hk/'>The Hong Kong University of Science and Technology</a> (HKUST). I'm advised by Prof. <a href='https://www.cse.ust.hk/~weiwa/'>Wei Wang</a>.
-
-Before that, I spent a wonderful gap year as a Research Assistant at <a href='https://ipads.se.sjtu.edu.cn/'>Institute of Parallel and Distributed Systems</a> (IPADS) in Shanghai Jiao Tong University, advised by Prof. <a href='https://ipads.se.sjtu.edu.cn/pub/members/zhaoguo_wang'>Zhaoguo Wang</a>.
-
-I enjoyed a three-month research advised by Prof. <a href='https://naizhengtan.github.io/'>Cheng Tan</a> from Northeastern University in 2022.
-
-I received my B.S. in Computer Science and Engineering in <a href='https://www.osu.edu/'>The Ohio State University</a>. Once a Buckeye, Always a Buckeye.
-
-I have broad research interests in **distributed system**, **system for ML**, **cloud computing**, and other related stuff.
---
+++ assets/js/search-data.js
@@ -0,0 +1,61 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-publications",
+          title: "publications",
+          description: "publications by categories in reversed chronological order.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "news-webridge-accpeted-by-sigmod-24",
+          title: 'WeBridge accpeted by SIGMOD’24.',
+          description: "",
+          section: "News",},{id: "news-research-intern-at-alibaba-cloud",
+          title: 'Research Intern at Alibaba Cloud.',
+          description: "",
+          section: "News",},{id: "news-i-will-serve-as-a-member-of-sigmod-24-ari-committee",
+          title: 'I will serve as a member of SIGMOD’24 ARI Committee.',
+          description: "",
+          section: "News",},{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=bEtLSugAAAAJ", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
